233245aa01f1791b607496487ced788a4742c712
gjwoo96 <gjwoo96@naver.com>
2022-10-27 13:27:35 +0900

    qna영역 수정

--- start/js/start.js
+++ start/js/start.js
@@ -4,6 +4,31 @@
 const main = document.querySelector("#main");
 const qna = document.querySelector("#qna");
 
+function addAnswer(answerTEXT ,qIdx){
+  var a = document.querySelector(".answerBox");
+  var answer = document.createElement("button");
+  answer.classList.add("answerList");
+  a.appendChild(answer);
+  answer.innerHTML = answerTEXT;
+  answer.addEventListener("click",function(){
+    //querySelectorAll 해당하는 요소 전체를 가져옴
+    var children = document.querySelectorAll(".answerList");
+    for(let i = 0; i< children.length; i++){
+      children[i].disabled = true;
+      children[i].style.display= "none";
+    }
+    
+    goNext(++qIdx);
+  },false);
+}
+function goNext(qIdx){
+  var q = document.querySelector(".qBox");
+  q.innerHTML = qnaList[qIdx].q;
+  for(let i in qnaList[qIdx].a){
+    addAnswer(qnaList[qIdx].a[i].answer, qIdx);
+  }
+}
+
 function begin(){
   main.style.webkitAnimaition = "fadeOut 1s";
   main.style.animation = "fadeOut 1s";
@@ -15,5 +40,9 @@ function begin(){
       main.style.display = "none";
       qna.style.display="block";
     },450)
+    let qIdx = 0;
+    goNext(qIdx);
   }, 450);
 }
+
+
